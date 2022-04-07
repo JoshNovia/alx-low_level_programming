1900cfff05667640aa094ba4466fb6a21b15d976
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-07 13:53:11 -0700

    commit for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main(void)
+/**
+ * main entry point
+ *
+ *Return: 0 after printing function
+ */
+int main (void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return(0);
+puts ("\" "Programming is like building a multilingual puzzle");
+return (0);
 }	 
 
