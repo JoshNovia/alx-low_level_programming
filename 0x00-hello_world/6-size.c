6305e7e8665729d222faeac69c04fe5279bc4d84
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-08 06:53:39 -0700

    commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
+#include <stdio.h>
 /**
 *
-*main entry point
-*
-*Return: Always return o (Success)
+*main entry
+*Return: Always 0 (Success)
 */
-#include <stdio.h>
 int main(void)
-}
-printf("Size of char: %d byte(s)\n", sizeof(char));
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long: %d bytes(s)\n", sizeof(long int));
-printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: &d byte(s)\n", sizeof(float);
-return(0);		
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("size of a float: %d bytes(s)\n, sizeof(float));
+return(0);
