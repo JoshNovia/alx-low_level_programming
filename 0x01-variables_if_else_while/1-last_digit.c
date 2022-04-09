cb888a91590983c4543b50a5c699d9065bf3f381
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-09 07:03:48 -0700

    Commit for task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
@@ -24,7 +24,7 @@ else if (n % 10 == 0
 {
 printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
-
+i
 else
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
