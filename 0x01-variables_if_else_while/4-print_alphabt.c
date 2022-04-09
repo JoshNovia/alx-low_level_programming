03bf634551efa09f8601be2ff6cddee46fbbfeb9
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-09 08:31:25 -0700

    commit for task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/**
+*main - Entry point
+*
+*Return: Always 0 (Succes/Correct)
+*/
+
+int main(void)
+
+{
+char alphabet:
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar( '\n' );
+
+return (0);
+}
