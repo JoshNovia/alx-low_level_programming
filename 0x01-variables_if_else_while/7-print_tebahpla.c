0eb837723ac287eeb89623f40ae1711bbb19f7c2
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-11 11:06:34 -0700

    My commit message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success/Correct)
+*/
+
+int main(void)
+{
+char alphabet;
+for (alphabet = 22 ; >=97; alphabet==)
+{
+putchar(alphabet);
+}
+
+putchar( '\n' );
+
+return (0);
+
+}
