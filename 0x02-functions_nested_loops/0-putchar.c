2589c42d5cb9a979a598b0db3ebce3184bde2921
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-14 04:24:10 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
  */
 int void(main)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
