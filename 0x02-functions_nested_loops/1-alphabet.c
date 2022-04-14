10b29bdc63004ce082557f730a9796d04deabd21
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-14 04:33:23 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,20 @@
 
 
 /**
- * prints alphabet: prints alphabet in lowercase followed by a new line
+ * prints alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z' letter++)
-_putchar(letter);
+char c ='a';
+while (c <= 'z')
+}
+_putchar(c);
+c++;
+}
 
 _putchar('\n');
+
 return (0);
 }
