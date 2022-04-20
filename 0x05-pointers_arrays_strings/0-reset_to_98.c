12b8da0cf22f37b00079bb445063490aef7e6d6f
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-20 11:10:29 -0700

    task zero commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update an interger value to 98
+ * @n: what points to an int to be updated
+ *
+ * Return: 0, that means it is correct
+ */
+
+void reset_to_98(int *n);
+{
+	int *n = 98;
+}
