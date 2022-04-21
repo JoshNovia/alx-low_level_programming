5b8fefad194497d9c19ce23723b6494ac8598e1d
Ndukwu Chukwudalu Joshua <chuckjoshnovia@gmail.com>
2022-04-21 09:28:04 -0700

    my task two commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: the pointer
+ * Return: count length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	for(int *s; *s != '\0'; s++)
+	{
+		count++;
+	}
+
+	return (count);
+}
